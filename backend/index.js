7a105c355df982f280bf2c9d4dfaba288b4a2b46
Sivaranjani-Nagarajan <sivaranjanint@gmail.com>
2025-09-15 12:18:58 +0530

    Bulk-Mail project

---
+++ backend/index.js
@@ -0,0 +1,85 @@
+import express from "express"
+import cors from "cors"
+import nodemailer from "nodemailer"
+import mongoose from "mongoose"
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Connected to DB"))
+.catch(err => console.error("Failed to connect:", err));
+
+
+
+const credentialSchema = new mongoose.Schema({}, { strict: false })
+
+
+const credential = mongoose.model("credential",credentialSchema,"bulkmail")
+
+
+app.post("/sendemail", function (req, res) {
+
+    let msg = req.body.msg
+    let emailList = req.body.emailList
+
+   
+credential.find().then(function(data){
+
+    const transporter = nodemailer.createTransport({
+
+    service: "gmail",
+
+    auth: {
+        user: data[0].toJSON().user,
+        pass: data[0].toJSON().pass,
+    },
+});
+
+new Promise(async function (resolve, reject) {
+
+        try {
+
+            for (var i = 0; i < emailList.length; i++) {
+
+               await transporter.sendMail({
+                    from: "sivaranjanint@gmail.com",
+                    to: emailList[i],
+                    subject: "A message from Bulk Mail App",
+                    text: msg
+                }
+
+                )
+
+                console.log("Email sent to:"+emailList)
+            }
+
+            resolve("Success")
+
+        }
+        catch (error) {
+            res.send(fail)
+        }
+    }).then(function(){
+        res.send(true)
+    }).catch(function(){
+        res.send(false)
+    })
+
+
+}).catch(function(error){
+    console.log(error)
+})
+
+
+})
+
+app.listen(5000, function () {
+    console.log("Server Started ...")
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import axios from 'axios'
+import * as XLSX from "xlsx"
+
+function App() {
+
+  const [msg, setmsg] = useState("")
+  const [status, setstatus] = useState("")
+  const [emailList, setemailList] = useState([])
+
+  function handlemsg(evt){
+    setmsg(evt.target.value)
+  }
+
+  function send (){
+    setstatus(true)
+    axios.post("http://localhost:5000/sendemail", {msg:msg,emailList:emailList})
+    .then(function(data){
+      if (data.data === true){
+        alert("Email Sent Successfully")
+        setstatus(false)
+      }
+      else{
+        alert("Failed")
+      }
+    })
+  }
+
+  function handlefile(event){
+
+    const file = event.target.files[0]
+    console.log(file)
+
+    const reader = new FileReader()
+
+    reader.onload = function (event) {
+        const data = event.target.result
+        const workbook = XLSX.read(data, { type: "array" })
+        const sheetName = workbook.SheetNames[0]
+        const worksheet = workbook.Sheets[sheetName]
+        const emailList = XLSX.utils.sheet_to_json(worksheet,{header:'A'})
+        const totalemail = emailList.map(function(item){return item.A})
+        console.log(totalemail)
+        setemailList(totalemail)
+    }
+
+
+    reader.readAsArrayBuffer(file)
+  }
+
+  return (
+    <>
+      <div className='bg-violet-950 text-white text-center'>
+        <h1 className='text-2xl font-medium px-5 py-3'>BulkMail</h1>
+      </div>
+
+      <div className='bg-violet-800 text-white text-center'>
+        <h1 className='font-medium px-5 py-3'>we can help you business with sending multiple emails at once</h1>
+      </div>
+
+      <div className='bg-violet-600 text-white text-center'>
+        <h1 className='font-medium px-5 py-3'>Drag and Drop</h1>
+      </div>
+
+      <div className='bg-violet-400 flex flex-col items-center px-5 py-3'>
+        <textarea onChange={handlemsg} value={msg} className='w-96 h-96 py-2 outline-none px-2 border-black rounded-md' placeholder='Enter your text here ...'></textarea>
+
+        <div>
+          <input type="file" onChange={handlefile} className='border border-dashed py-4 px-4 mt-5 mb-5' />
+        </div>
+
+        <p>Total Emails in the file: {emailList.length} </p>
+
+        <button onClick={send} className='bg-blue-950 py-2 px-2 text-white font-medium rounded-md w-fit mt-2'>{status?"Sending":"Send"}</button>
+
+      </div>
+
+      
+    </>
+  )
+}
+
+export default App
