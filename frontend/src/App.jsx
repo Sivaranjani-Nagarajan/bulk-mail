ec4c1204c1ca939af41ae4da0c939e9287b3ab42
Sivaranjani-Nagarajan <sivaranjanint@gmail.com>
2025-09-15 15:29:04 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   function send (){
     setstatus(true)
-    axios.post("https://bulk-mail-h73k.onrender.com/sendemail", {msg:msg,emailList:emailList})
+    axios.post("https://bulk-mail-h73k.onrender.com/sendemail", { msg, emailList })
     .then(function(data){
       if (data.data === true){
         alert("Email Sent Successfully")
